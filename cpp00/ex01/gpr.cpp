9783c77568f1cb9921763d487c34cc33e5efcc10
slo <selbihafid@gmail.com>
2023-01-04 00:40:14 +0100

    last touch need it

--- cpp00/ex01/gpr.cpp
+++ cpp00/ex01/gpr.cpp
@@ -1,78 +1,26 @@
 #include <iostream>
 #include <string>
-// using namespace std;
-
-// class person {
-// private:
-//     string name;
-//     int age;
-
-// public:
-//     // default constructor
-//     person()
-//     {
-//         name = "N/A";
-//         age = 0;
-//     }
-
-//     // parameterized constructor with 
-//     // default argument
-//     person(string name, int age = 18)
-//     {
-//         this->name = name;
-//         this->age = age;
-//     }
-
-//     // function to display values
-//     void display()
-//     {
-//         cout << name << "\t" << age << endl;
-//     }
-// };
-
-// int main()
-// {
-//     //array of class objects declaration
-//     person per[4] = { person("ABC"), person("PQR"), person("XYZ", 30), person() };
-
-//     per[0].display();
-//     per[1].display();
-//     per[2].display();
-//     per[3].display();
-
-//     return 0;
-// }
-
-
-class book
-{
-public:
-  std::string name;
-  std::string lname; 
-  void print()
-  {
-    std::cout << "name ==>" << name << std::endl;
-    std::cout << "last name ==>" << lname << std::endl;
-  }
-  book()
-  {
-    name = "nothing";
-    lname = "last nothing";
-  }
-   book()
-  {
-    name = "nothing";
-    lname = "last nothing";
-  }
-};
 
 int main()
 {
-  book sq[3];
-  for (int i = 0; i < 3; i++)
+  std::string name;
+  while (std::getline(std::cin, name))
   {
-    sq[i].print();
+    if (!name.compare("ADD"))
+    {
+      std::cout << "here i am adding" << std::endl;
+    }
+    else if (!name.compare("SEARCH"))
+    {
+      std::cout << "here i am in searching" << std::endl;
+    }
+    else if (!name.compare("EXIT"))
+    {
+      std::cout << "here i am in exit" << std::endl;
+      exit(0);
+    }
   }
+  
   
   return 0;
 }
