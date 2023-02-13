6837830a6b5db9b590f050648874b6b0e3a78935
slo <selbihafid@gmail.com>
2023-02-13 15:11:08 +0100

    empthy

---
+++ cpp05/ex00/push/Bureaucrat.hpp
@@ -0,0 +1,24 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+private:
+    /* data */
+public:
+    Bureaucrat(/* args */);
+    ~Bureaucrat();
+};
+
+Bureaucrat::Bureaucrat(/* args */)
+{
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
+
+
+#endif
